11d23d839ef36412a1a939b0d1e8e03f92ba31cd
tktwr <takehiro.tawara@gmail.com>
2021-09-29 08:31:06 +0900

    update

---
+++ make.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#======================================================
+# functions
+#======================================================
+f_default() {
+  f_tags_memo
+}
+
+f_tags_memo() {
+  memotags.sh *.html > tags.memo
+}
+
+f_helptags() {
+  vim -e -c "helptags . | quit"
+}
+
+f_dot() {
+  dot -T svg -o images/skk.svg skk.dot
+  dot -T svg -o images/git.svg git.dot
+  dot -T svg -o images/vim-ide.svg vim-ide.dot
+}
+
+f_help() {
+	echo "tags_memo"
+	echo "helptags"
+	echo "dot"
+	echo "help"
+}
+
+#======================================================
+# main
+#======================================================
+func_name=${1:-"default"}
+eval "f_$func_name"
+
