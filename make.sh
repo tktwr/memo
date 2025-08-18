26ef10ace269a6e4912cc6e0ccb7a968b6ba2539
tktwr <takehiro.tawara@gmail.com>
2025-08-18 20:34:47 +0900

    update

--- make.sh
+++ make.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 source cmd_git.sh
+source cmd_util.sh
 
 #======================================================
 # functions
 #======================================================
-f_0_submod_add()    { f_git_submodule_add_memo_js; }
-f_0_submod_update() { f_git_submodule_update_all; }
-f_0_submod_status() { f_git_submodule_status; }
+f_init()   { f_util_add_memo_js; }
+f_update() { f_git_submodule_update_all; }
+f_status() { f_git_submodule_status; }
+f_tags()   { f_util_tags; }
 
 f_dot() {
   dot -T svg -o images/skk.svg dot/skk.dot
   dot -T svg -o images/git.svg dot/git.dot
   dot -T svg -o images/vim-ide.svg dot/vim-ide.dot
 }
-
-f_tags() { memotags.sh 'memo:' *.html > tags; }
 #======================================================
 # main
 #======================================================
