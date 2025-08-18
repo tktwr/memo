a1e1765781ff59a09248c4db2b036353f99038ea
tktwr <takehiro.tawara@gmail.com>
2025-08-18 11:24:41 +0900

    update

--- make.sh
+++ make.sh
@@ -7,6 +7,13 @@ f_add_submodule() {
   mkdir -p third_party
   git submodule add https://github.com/tktwr/memo_js.git third_party/memo_js
 }
+f_git_submodule_update() {
+  git submodule update --init --recursive --remote --merge
+}
+f_git_submodule_status() {
+  git submodule status --recursive
+  git submodule summary
+}
 f_tags() {
   memotags.sh 'memo:' *.html > tags
 }
