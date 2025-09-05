c85b556029680f15c7d932fcb412071a08486bb3
tktwr <takehiro.tawara@gmail.com>
2025-09-06 05:00:44 +0900

    update

--- make.sh
+++ make.sh
@@ -6,12 +6,12 @@ source cmd_util.sh
 #======================================================
 # functions
 #======================================================
-f_init()   { f_util_add_memo_js; }
-f_update() { f_git_submodule_update_all; }
-f_status() { f_git_submodule_status; }
-f_tags()   { f_util_tags; }
+f_01_status() { f_git_submodule_status; }
+f_02_update() { f_git_submodule_update_all; }
+f_03_tags()   { f_util_tags; }
+f_04_init()   { f_util_add_memo_js; }
 
-f_dot() {
+f_11_dot() {
   dot -T svg -o images/skk.svg dot/skk.dot
   dot -T svg -o images/git.svg dot/git.dot
   dot -T svg -o images/vim-ide.svg dot/vim-ide.dot
